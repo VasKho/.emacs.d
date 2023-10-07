be8c47ff33f2ba9d7765cb51172f256482f80bc6
VasKho <vasya.khoroshavin@gmail.com>
2023-10-07 17:58:52 +0300

    Change startup order

--- lisp/config/dired.el
+++ lisp/config/dired.el
@@ -4,52 +4,6 @@
 
 (require 'cl-lib)
 (require 'nerd-icons-dired)
-(require 'dired-sidebar)
-
-(defun dired--set-root-directory (path)
-  "Set Dired root directory to PATH."
-  (dired-sidebar-with-no-dedication
-   (let ((name (dired-sidebar-buffer-name path)))
-     (if (dired-sidebar-buffer-exists-p name)
-         (switch-to-buffer name)
-       (progn
-	 (find-alternate-file path)
-	 (dired-sidebar-mode)))
-     (dired-sidebar-update-state (current-buffer)))))
-
-(defun dired-sidebar-toggle ()
-  "Toggle dired-sidebar."
-  (interactive)
-  (if (dired-sidebar-showing-sidebar-p)
-      (progn
-	(dired-sidebar-hide-sidebar)
-	(cl-loop for buff in dired-buffers do (kill-buffer (cdr buff))))
-    (let ((buffer (dired-sidebar-get-or-create-buffer (ws-selector-get-working-directory))))
-      (dired-sidebar-show-sidebar buffer)
-      (pop-to-buffer (dired-sidebar-buffer)))))
-
-(defun dired-sidebar-subtree-toggle ()
-  "Toggle subtree in dired-sidebar with nerd icons."
-  (interactive)
-  (dired-subtree-toggle)
-  (nerd-icons-dired--refresh))
-
-(defun dired-up-dir ()
-  "Set root directory of Dired sidebar to parent directory."
-  (interactive)
-  (ws-selector--set-frame-working-directory
-   (selected-frame)
-   (file-name-directory (directory-file-name (dired-current-directory))))
-  (dired--set-root-directory (ws-selector-get-working-directory)))
-
-(defun dired-enter-dir ()
-  "Set root directory of Dired sidebar to directory at point."
-  (interactive)
-  (when (directory-name-p (dired-file-name-at-point))
-    (ws-selector--set-frame-working-directory
-     (selected-frame)
-     (dired-get-filename))
-    (dired--set-root-directory (ws-selector-get-working-directory))))
 
 (use-package dired
   :hook (dired-mode . dired-omit-mode))
@@ -62,6 +16,51 @@
   (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
   (setq dired-sidebar-use-term-integration t)
   (setq dired-sidebar-close-sidebar-on-file-open t)
+
+  (defun dired--set-root-directory (path)
+    "Set Dired root directory to PATH."
+    (dired-sidebar-with-no-dedication
+     (let ((name (dired-sidebar-buffer-name path)))
+       (if (dired-sidebar-buffer-exists-p name)
+           (switch-to-buffer name)
+	 (progn
+	   (find-alternate-file path)
+	   (dired-sidebar-mode)))
+       (dired-sidebar-update-state (current-buffer)))))
+
+  (defun dired-sidebar-toggle ()
+    "Toggle dired-sidebar."
+    (interactive)
+    (if (dired-sidebar-showing-sidebar-p)
+	(progn
+	  (dired-sidebar-hide-sidebar)
+	  (cl-loop for buff in dired-buffers do (kill-buffer (cdr buff))))
+      (let ((buffer (dired-sidebar-get-or-create-buffer (ws-selector-get-working-directory))))
+	(dired-sidebar-show-sidebar buffer)
+	(pop-to-buffer (dired-sidebar-buffer)))))
+
+  (defun dired-sidebar-subtree-toggle ()
+    "Toggle subtree in dired-sidebar with nerd icons."
+    (interactive)
+    (dired-subtree-toggle)
+    (nerd-icons-dired--refresh))
+
+  (defun dired-up-dir ()
+    "Set root directory of Dired sidebar to parent directory."
+    (interactive)
+    (ws-selector--set-frame-working-directory
+     (selected-frame)
+     (file-name-directory (directory-file-name (dired-current-directory))))
+    (dired--set-root-directory (ws-selector-get-working-directory)))
+
+  (defun dired-enter-dir ()
+    "Set root directory of Dired sidebar to directory at point."
+    (interactive)
+    (when (directory-name-p (dired-file-name-at-point))
+      (ws-selector--set-frame-working-directory
+       (selected-frame)
+       (dired-get-filename))
+      (dired--set-root-directory (ws-selector-get-working-directory))))
   
   :bind
   (:map dired-sidebar-mode-map
--- lisp/config/slime.el
+++ lisp/config/slime.el
@@ -2,6 +2,15 @@
 ;;; Commentary:
 ;;; Code:
 
+(use-package slime-company
+  :ensure t
+  :after (slime company)
+  :config
+  (setq slime-company-completion 'simple)
+  (setq slime-company-after-completion nil)
+  (setq slime-company-display-flags nil)
+  (setq slime-company-complete-in-comments-and-strings t))
+
 (use-package slime
   :ensure t
   :pin melpa
@@ -22,15 +31,6 @@
      (dolist (h '(slime-mode-hook slime-repl-mode-hook sldb-mode-hook))
        (remove-hook h 'slime-company-maybe-enable))
      (slime-company-disable))))
-
-(use-package slime-company
-  :ensure t
-  :after (slime company)
-  :config
-  (setq slime-company-completion 'simple)
-  (setq slime-company-after-completion nil)
-  (setq slime-company-display-flags nil)
-  (setq slime-company-complete-in-comments-and-strings t))
 
 (provide 'slime-config)
 ;;; slime.el ends here
